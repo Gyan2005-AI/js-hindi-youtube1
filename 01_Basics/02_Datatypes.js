22eccac266c9230fe25801a2596633b48a8672ef
Gyan2005-AI <igyanchandra2006@gmail.com>
2024-10-27 22:52:24 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3) // we are using NodeJS not browser
+
+console.log(3 +
+    3) // code readablity should be high
+
+console.log("Gyanchandra")
+
+
+
+let name ="Gyanchandra"
+let age = 18
+let loggedIn = false
+
+
+
+
+// number => 2 to power 53
+// bigint is used in vast projects
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
