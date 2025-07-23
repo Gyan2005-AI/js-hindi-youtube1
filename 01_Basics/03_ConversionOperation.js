bca4f0a670b12f9fc9060f8da9deca374c06b381
Gyan2005-AI <igyanchandra2006@gmail.com>
2025-07-23 12:51:43 +0530

    Update 03_ConversionOperation.js

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -35,7 +35,7 @@ console.log(negValue);
 // console.log(2*2);
 // console.log(2**3);
 // console.log(2/3);
-// console.log(2%2);
+// console.log(2%3);
 
 let str1 = "hello"
 let str2 = " Gyanchandra"
@@ -43,7 +43,7 @@ let str3 = str1 + str2
 console.log(str3);
 
 // console.log("1" + 2);
-// console.log(2 + "1");
+// console.log(1 + "2");
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
 
