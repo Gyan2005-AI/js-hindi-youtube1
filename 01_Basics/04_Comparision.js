e90d8d85f6afbf991341711f390b70523b54f156
Gyan2005-AI <igyanchandra2006@gmail.com>
2025-07-23 13:04:39 +0530

    Update 04_Comparision.js

--- 01_Basics/04_Comparision.js
+++ 01_Basics/04_Comparision.js
@@ -1,4 +1,4 @@
-console.log( 2 > 1 );
+console.log( 2 > 1 ); // Output will be in boolean (True and False)
 console.log( 2 >= 1 );
 console.log( 2 < 1 );
 console.log( 2 == 1 );
@@ -26,7 +26,7 @@ console.log( undefined < 0 );
 
 // ===
 
-console.log( "2" === 2 )
+console.log( "2" === 2 ) // === checks strictly the value as well as datatype
 
 
 
