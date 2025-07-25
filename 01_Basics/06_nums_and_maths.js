8315f0e18fe478887e42fef856dadced856ea2a1
Gyan2005-AI <igyanchandra2006@gmail.com>
2025-07-25 22:22:23 +0000

    Nums_and_Maths

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,41 @@
+const score = 400 ;
+console.log(score) ;
+
+const balance = new Number(100) ;
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); // used in Ecommerce
+
+const otherNumber = 123.8966;
+console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); // Gives the absolute value -4 =4 only
+console.log(Math.round(4.6)); // roundoff the value
+console.log(Math.ceil(4.2)); //  Gives upper value
+console.log(Math.floor(4.9)); // Gives lower Value
+console.log(Math.max(4,5,6,7));
+console.log(Math.min(4,5,6,7));
+
+console.log(Math.random()); // Gives value under 0 and 1
+console.log((Math.random() * 10) +1);
+console.log(Math.floor(Math.random() *10)+ 1);
+ const min =10;
+ const max = 20;
+
+ console.log(Math.floor (Math.random () * (max - min +1)) + min ) //it gives value from 10 and 20 
+
+
+
+
+
+
+
+
+
