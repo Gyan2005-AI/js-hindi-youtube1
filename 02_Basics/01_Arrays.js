b08fd72b9574a6a02ab3a6328d1a72bafd5f5126
Gyan2005-AI <igyanchandra2006@gmail.com>
2025-08-03 21:48:43 +0000

    Array Part-1

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,46 @@
+// Array
+
+const myArr = [0,1,2,3,4,5, true, "hitesh"] //values in square box called elements 
+const myHeros = ["Shaktimaan", "Naagraj"]
+const myArr2 = new Array(1,2,3,4)
+
+console.log(myArr[1]);
+
+// Array 
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(9) 
+myArr.shift() 
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// const newArr = myArr.join() // Join binds and get the value in string
+//     console.log(typeof newArr)
+    
+// slice and splice
+
+console.log("A" , myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B" , myArr);
+
+const myn2 = myArr.splice(1,3) // It manipulated the array
+console.log("C", myArr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+console.log(myArr);
+
+
